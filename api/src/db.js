a879e4a4b8fa6bc8ab4e410972be5cde47f69ea6
Diegoc24 <carusodiegod@gmail.com>
2023-07-18 23:42:47 -0300

    feature/backend(modificarion)

--- api/src/db.js
+++ api/src/db.js
@@ -2,14 +2,11 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-import pg from "pg"
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
+  POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_HOST, POSTGRES_DATABASE, DB_PORT
 } = process.env;
 
-const {Pool} = pg
-
-const sequelize = new Sequelize(process.env.POSTGRES_URL_NON_POOLING, {
+const sequelize = new Sequelize(`postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${DB_PORT}/${POSTGRES_DATABASE}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
