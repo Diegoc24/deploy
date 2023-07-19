db78cb86a92039155195944f919531ff62ba1818
Diegoc24 <carusodiegod@gmail.com>
2023-07-18 22:42:33 -0300

    feature/backend(modificarion)

--- api/src/db.js
+++ api/src/db.js
@@ -2,21 +2,14 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const pg = require("pg")
+
 const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, DB_DEBUG
 } = process.env;
 
-const {Pool} = pg
-let sequelize
-if(DB_DEBUG === "true"){
-sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
-}else{
-  sequelize = new Sequelize(process.env.POSTGRES_URL)
-}
+
+
+ const sequelize = new Sequelize(process.env.POSTGRES_URL)
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
