712382153319bc693caa86b593392c97dcb4b2f4
Diegoc24 <carusodiegod@gmail.com>
2023-07-18 21:45:08 -0300

    feature/modificatio

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 require("dotenv").config();
 
-const {PORT} = process.env
+const PORT = process.env.PORT || 3001
 // Syncing all the models at once.
 conn.sync({ alter: true }).then(() => {
   server.listen(PORT, () => {
