3bbf3a68efb8c449872cfda22b0ad39116be58c9
Diegoc24 <carusodiegod@gmail.com>
2023-07-23 18:56:30 -0300

    feature/Fontend(modificationDetail)

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -37,10 +37,13 @@ const GetPlat = ()=>{
         
         
         <div className="containerDetail">
-           <NavLink className={"backHome"} to={"/home"}><img src="https://cdn-icons-png.flaticon.com/512/566/566095.png" alt="back"/></NavLink>
+            
+           <NavLink className={"backHome"} to={"/home"}><svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"/>
+</svg></NavLink>
             {
                Object.keys(game).length !== 0 ? 
-                <div>
+                <div className="containerDetailInter">
                 
              <h1>{game.name}</h1>
             <img className="imageDetail" src={game.background_image} alt={game.name}/>
