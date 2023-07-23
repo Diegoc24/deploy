967d43f11058fb11cfe7f38adf7d2dad3ec6c8d0
Diegoc24 <carusodiegod@gmail.com>
2023-07-23 16:59:18 -0300

    feature/Backend(modificationApp)

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Detail from "./components/Detail/Detail";
 import Form from "./components/form/Form";
 import GameCreated from "./components/GameCreated/GameCreated";
 import axios from "axios";
-axios.defaults.baseURL = "https://backend-videogames-l2xp.onrender.com/"
+axios.defaults.baseURL = "https://backend-videogames-l2xp.onrender.com"
 
 function App() {
  
